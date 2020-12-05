6832da0af4cb15ace30cc804e24c455cb35bb3bd
john lee <tanjongpagar0501@gmail.com>
2020-12-05 17:16:22 +0800

    added latest file

--- models/item.py
+++ models/item.py
@@ -18,18 +18,13 @@ class ItemModel(db.Model):
     def json(self):
         return {'name': self.name, 'price': self.price}
 
-
     @classmethod
     def find_by_name(cls, name):
         return cls.query.filter_by(name=name).first()
 
-
     def save_to_db(self):
         db.session.add(self)
         db.session.commit()
-
-
-
 
     def delete_from_db(self):
         db.session.delete(self)
--- models/store.py
+++ models/store.py
@@ -12,12 +12,11 @@ class StoreModel(db.Model):
         self.name = name
 
     def json(self):
-        return {'name': self.name, 'items': [item.json() for item in self.item.all()]}
+        return {'name': self.name, 'items': [item.json() for item in self.items.all()]}
 
     @classmethod
     def find_by_name(cls, name):
         return cls.query.filter_by(name=name).first()
-
 
     def save_to_db(self):
         db.session.add(self)
